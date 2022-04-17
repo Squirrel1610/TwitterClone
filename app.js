1b998a4d4d6edd81a3974873b0448ce990a18ede
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-04-18 00:13:34 +0700

    template login, register

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
+const { dirname } = require("path");
 const port = 3000;
 const app = express();
+const path = require("path");
 
 //template engine
 app.set("view engine", "pug");
 app.set("views", "views");
 
+//static file
+app.use(express.static(path.resolve(__dirname, "public")));
+
 //middleware
 const middleware = require("./middleware");
 
 //routes
+const registerRoute = require("./routes/registerRoutes");
 const loginRoute = require("./routes/loginRoutes");
 
+app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 
 app.get("/", middleware.requireLogin, (req, res) => {
