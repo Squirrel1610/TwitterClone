526d4fc13748173c25f55832d4b4510ec838ed1a
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-04-22 10:15:28 +0700

    styling the post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const { route } = require("../registerRoutes");
 const router = express.Router();
+const Post = require("../../schemas/PostSchema");
+const User = require("../../schemas/UserSchema");
 
 router.post("/", async (req, res, next) => {
   if (!req.body.content) {
     console.log("Content param not sent with request");
     return res.sendStatus(400);
   }
-  res.status(200).send("It worked");
+
+  var postData = {
+    content: req.body.content,
+    postedBy: req.session.user,
+  };
+
+  Post.create(postData)
+    .then(async (newPost) => {
+      newPost = await User.populate(newPost, { path: "postedBy" });
+      res.status(201).send(newPost);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
 });
 
 module.exports = router;
