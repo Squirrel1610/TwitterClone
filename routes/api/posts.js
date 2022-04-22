943947c137b6b39810487f9fdb3dfb2a3500517e
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-04-22 13:01:59 +0700

    output posts on home page

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const Post = require("../../schemas/PostSchema");
 const User = require("../../schemas/UserSchema");
 
+//select all tweet
+router.get("/", async (req, res, next) => {
+  Post.find({})
+    .populate("postedBy")
+    .sort({ createdAt: -1 })
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+//post tweet
 router.post("/", async (req, res, next) => {
   if (!req.body.content) {
     console.log("Content param not sent with request");
