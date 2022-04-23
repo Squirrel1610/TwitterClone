39162e98bde92f12b43e5e57be725f26a89c125a
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-04-23 17:18:37 +0700

    finish like

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get("/", middleware.requireLogin, (req, res) => {
   var payload = {
     pageTitle: "Home",
     userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
   };
 
   res.status(200).render("home", payload);
