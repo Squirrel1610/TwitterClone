ced748070fd511c6d0c9791450050a4451c55784
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2022-05-02 22:35:53 +0700

    cover photo

--- public/js/common.js
+++ public/js/common.js
@@ -202,6 +202,27 @@ $("#filePhoto").change(function () {
   }
 });
 
+$("#coverPhoto").change(function () {
+  if (this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      var image = document.getElementById("coverPreview");
+      image.src = e.target.result;
+
+      if (cropper !== undefined) {
+        cropper.destroy();
+      }
+
+      cropper = new Cropper(image, {
+        aspectRatio: 16 / 9,
+        background: true,
+      });
+    };
+    reader.readAsDataURL(this.files[0]);
+  }
+});
+
+//crop and send profile picture to server
 $("#imageUploadButton").click((e) => {
   var canvas = cropper.getCroppedCanvas();
   if (canvas == null) {
@@ -215,6 +236,30 @@ $("#imageUploadButton").click((e) => {
 
     $.ajax({
       url: "/api/users/profilePicture",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: () => {
+        return location.reload();
+      },
+    });
+  });
+});
+
+$("#coverPhotoButton").click((e) => {
+  var canvas = cropper.getCroppedCanvas();
+  if (canvas == null) {
+    alert("Could not upload image. Make sure it is an image file.");
+    return;
+  }
+
+  canvas.toBlob((blob) => {
+    var formData = new FormData();
+    formData.append("croppedImage", blob);
+
+    $.ajax({
+      url: "/api/users/coverPhoto",
       type: "POST",
       data: formData,
       processData: false,
--- routes/api/users.js
+++ routes/api/users.js
@@ -69,6 +69,7 @@ router.get("/:userId/followers", async (req, res, next) => {
     });
 });
 
+//upload profile picture
 router.post(
   "/profilePicture",
   upload.single("croppedImage"),
@@ -91,6 +92,36 @@ router.post(
       req.session.user = await User.findByIdAndUpdate(
         req.session.user._id,
         { profilePic: filePath },
+        { new: true }
+      );
+      res.sendStatus(200);
+    });
+  }
+);
+
+//upload cover photo
+router.post(
+  "/coverPhoto",
+  upload.single("croppedImage"),
+  async (req, res, next) => {
+    if (!req.file) {
+      console.log("No file uploaded with ajax request");
+      return res.sendStatus(400);
+    }
+
+    var filePath = `/uploads/images/${req.file.filename}.png`;
+    var tempPath = req.file.path;
+    var targetPath = path.join(__dirname, `../../${filePath}`);
+
+    fs.rename(tempPath, targetPath, async (error) => {
+      if (error != null) {
+        console.log(error);
+        return res.sendStatus(400);
+      }
+
+      req.session.user = await User.findByIdAndUpdate(
+        req.session.user._id,
+        { coverPhoto: filePath },
         { new: true }
       );
       res.sendStatus(200);
